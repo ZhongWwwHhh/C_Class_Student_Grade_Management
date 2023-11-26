54a2714d16b6aee47ac15cb1b1e3f94ed2e44177
ZhongWwwHhh <zhongwwwhhh@outlook.com>
2023-11-26 14:09:56 +0800

    add color

--- Src/main.c
+++ Src/main.c
@@ -3,9 +3,12 @@
 #include "file.h"
 #include "count_func.h"
 
+#define CLOUR_ON          "\033[41;37m"
+#define CLOUR_OFF         "\033[0m"
+
 int main()
 {
-    printf("Student Performance Management System");
+    printf(CLOUR_ON"Student Performance Management System"CLOUR_OFF);
     
     return 0;
 }
